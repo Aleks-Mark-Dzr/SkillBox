60e318c801c99fdb06dcf2db5adb0be5bb5da72e
Aleks-Mark-Dzr <60552334+Aleks-Mark-Dzr@users.noreply.github.com>
2020-06-22 14:57:53 +0300

    add BasicSintax 2

--- 02_BuildAndResources/SkillboxKotlin/app/src/main/java/com/markov/skillboxkotlin/MainKotlin.kt
+++ 02_BuildAndResources/SkillboxKotlin/app/src/main/java/com/markov/skillboxkotlin/MainKotlin.kt
@@ -1,2 +1,41 @@
 package com.markov.skillboxkotlin
 
+fun main() {
+    val shortVal: Short =123
+    val intVal =1366561246
+    val longVal = 746812648L
+
+    val floatVal = 872682763876.2382376428464f
+    val doubleVal = 3423.63623456234
+
+   val booleanVal:Boolean = true
+   val booleanVal2:Boolean = false
+
+    val booleanVal3 = booleanVal || booleanVal2
+    val booleanVal4 = booleanVal.not() && booleanVal2
+
+    val isEqual = intVal == 5
+    val isGreater = intVal >= 5
+
+    val charVal: Char = '2'
+    val stringVal = "string"
+    val multistringVal = """
+        jhfdjfg
+        jjg
+        weuff
+    """.trimIndent()
+
+
+    val name = "Ivan"
+    val lastName = "Ivanov"
+    val isMale = false
+
+    val humanReadableName = "My name is ${name} and lastname ${lastName}"
+    val humanReadableExpanded = "${if (isMale) "His" else "Her"} name is $name and lastname $lastName"
+
+    print(humanReadableExpanded)
+
+    7234574f.toDouble()
+    1324.toString()
+    "123".toInt()
+}
