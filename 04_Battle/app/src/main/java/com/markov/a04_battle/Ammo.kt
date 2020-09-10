b1d70ac639c666fa5aa936d05c446ef67e16066e
Aleks-Mark-Dzr <60552334+Aleks-Mark-Dzr@users.noreply.github.com>
2020-09-10 14:51:34 +0300

    add Ammo

---
+++ 04_Battle/app/src/main/java/com/markov/a04_battle/Ammo.kt
@@ -0,0 +1,25 @@
+package com.markov.a04_battle
+
+import kotlin.random.Random
+
+enum class Ammo(
+
+    val damage: Int,
+    val criticalDamageChance: Int,
+    val criticalDamageRatio: Int
+
+) {
+    AMMO762x39PS(60, 30, 50),
+    AMMO762x39BP(48, 36, 70),
+    AMMO762x39NR(72, 24, 40)
+
+}
+
+fun Ammo.calculateDamage(): Int {
+    val chance = Random.nextInt(100)
+    if (chance > criticalDamageChance) {
+        return 0
+    } else {
+        return damage * criticalDamageRatio / 100
+    }
+}
