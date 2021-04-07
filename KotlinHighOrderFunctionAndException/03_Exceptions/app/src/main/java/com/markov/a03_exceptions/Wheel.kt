a8dbac45c50d67ea48f5b57cff9d206284a13b2d
Марков Алексей <aleks-mark-dzr@yandex.ru>
2021-04-07 16:32:55 +0300

    correct task 4, 5

--- KotlinHighOrderFunctionAndException/03_Exceptions/app/src/main/java/com/markov/a03_exceptions/Wheel.kt
+++ KotlinHighOrderFunctionAndException/03_Exceptions/app/src/main/java/com/markov/a03_exceptions/Wheel.kt
@@ -13,18 +13,18 @@ class Wheel {
     class IncorrectPressure : Exception()
 
     fun setPressure(value: Double) {
-        pressure.plus(2.2)
+
+        if (value <1.6){
+            throw TooLowPressure()
+        }
+        if (value > 2.5) {
+            throw TooHighPressure()
+        }
+
+         this.pressure = value
 
         if (value < 0 || value > 10.0) {
             throw IncorrectPressure()
-        }
-    }
-
-    fun check(value: Double) {
-        if (value <1.6){
-            throw TooLowPressure()
-        } else if (value > 2.5) {
-            throw TooHighPressure()
         }
     }
 }
