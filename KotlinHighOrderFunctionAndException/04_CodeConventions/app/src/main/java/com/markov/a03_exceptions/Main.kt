52be06d9f2ff6719f62c95c0a58abcf10c3316c6
Aleks-Mark-Dzr <60552334+Aleks-Mark-Dzr@users.noreply.github.com>
2021-06-06 09:39:20 +0300

    fix code formatting errors with formatKotlin

--- KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/com/markov/a03_exceptions/Main.kt
+++ KotlinHighOrderFunctionAndException/04_CodeConventions/app/src/main/java/com/markov/a03_exceptions/Main.kt
@@ -3,14 +3,14 @@ package com.markov.a03_exceptions
 fun main() {
 
     val wheel = Wheel()
-    try{
+    try {
         wheel.setPressure(12.6)
         println("Correct Pressure = ${wheel.pressure}")
-    } catch (e1: Wheel.TooHighPressure){
+    } catch (e1: Wheel.TooHighPressure) {
         println("Too high pressure")
-    } catch (e2: Wheel.TooLowPressure){
+    } catch (e2: Wheel.TooLowPressure) {
         println("Too low pressure")
-    } catch (e3: Wheel.IncorrectPressure){
+    } catch (e3: Wheel.IncorrectPressure) {
         println("Incorrect pressure")
     }
 }
