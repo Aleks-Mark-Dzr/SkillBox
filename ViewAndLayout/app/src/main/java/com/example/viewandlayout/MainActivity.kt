ffbda90c50e9132cfec940c13819a80c7d91f6ce
Марков Алексей <aleks-mark-dzr@yandex.ru>
2021-10-30 18:00:58 +0300

    add build.gradle

--- ViewAndLayout/app/src/main/java/com/example/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/example/viewandlayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.viewandlayout
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,7 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
-
-
 }
