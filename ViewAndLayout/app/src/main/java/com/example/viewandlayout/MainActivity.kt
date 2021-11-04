a2e7eee4ba6b3b59a5fc18548a24f2f553ce0c6e
Марков Алексей <aleks-mark-dzr@yandex.ru>
2021-11-04 14:57:16 +0300

    add View Binding, add listener

--- ViewAndLayout/app/src/main/java/com/example/viewandlayout/MainActivity.kt
+++ ViewAndLayout/app/src/main/java/com/example/viewandlayout/MainActivity.kt
@@ -3,13 +3,40 @@ package com.example.viewandlayout
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View.inflate
+import android.widget.Toast
+import com.example.viewandlayout.databinding.ActivityMainBinding
+import com.example.viewandlayout.databinding.ActivityMainBinding.inflate
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        textEmailAddress.
+        binding.textEmailAddress.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                binding.textEmailAddress.text = takeIf { it.isNotBlank }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+        })
+
+
+
+
+        binding.textEmailAddress.setOnClickListener{
+            Toast.makeText(this, "Введите email", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
